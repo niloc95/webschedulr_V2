a99f1c532b3470a912c5801eef363e6a0db15f45
Nilo Cara <nilo.cara@gmail.com>
2025-03-12 10:36:54 +0200

    initial commit

---
+++ application/config/app.php
@@ -0,0 +1,18 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| App Configuration
+|--------------------------------------------------------------------------
+|
+| Declare some of the global config values of @webSchedulr.
+|
+*/
+
+$config['version'] = '1.5.1'; // This must be changed manually.
+
+$config['url'] = Config::BASE_URL;
+
+$config['debug'] = Config::DEBUG_MODE;
+
+$config['cache_busting_token'] = 'WS022025_v3'; // This must be changed manually.
